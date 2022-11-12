709f074e845747696a00c6b61678043dfa6271b8
Stefan Berggren <nsg@nsg.cc>
2022-11-13 00:08:43 +0100

    change path

--- script.sh
+++ script.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-while [ ! -e feed2toot_clientcred.txt ]; do
-    echo "Wait for feed2toot_clientcred.txt"
+while [ ! -e /feed2toot/feed2toot_clientcred.txt ]; do
+    echo "Wait for /feed2toot/feed2toot_clientcred.txt"
     sleep 10
 done
 
-while [ ! -e feed2toot.ini ]; do
-    echo "Wait for feed2toot.ini"
+while [ ! -e /feed2toot/feed2toot.ini ]; do
+    echo "Wait for /feed2toot/feed2toot.ini"
     sleep 10
 done
 
 while [ 1 ]; do
-    feed2toot -c feed2toot.ini
+    feed2toot -c /feed2toot/feed2toot.ini
     sleep 300
 done
