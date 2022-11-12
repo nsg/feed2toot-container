2c089af440deadd45df75102cf7426559c621d84
Stefan Berggren <nsg@nsg.cc>
2022-11-12 23:30:22 +0100

    in theory this should work

--- script.sh
+++ script.sh
@@ -2,10 +2,12 @@
 
 while [ ! -e feed2toot_clientcred.txt ]; do
     echo "Wait for feed2toot_clientcred.txt"
-    sleep 10;
+    sleep 10
 done
 
-echo "Main loop"
-while [ 1 ]; do
-    sleep 60;
+while [ ! -e feed2toot.ini ]; do
+    echo "Wait for feed2toot.ini"
+    sleep 10
 done
+
+feed2toot -c feed2toot.ini
